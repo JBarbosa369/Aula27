f6c91585275c44c7630de953e745d44f45a12c79
jeovana do maxwell <jeovana.nascimento@aluno.senai.br>
2024-11-28 09:53:31 -0500

    verificando os criterios de aprovãção/reprovação

--- Repositorio.py
+++ Repositorio.py
@@ -8,3 +8,18 @@ Notas = float(input("digite a nota [i+1] do aluno:"))
 Quantidade_faltas = int(input("digite a quantidade de faltas do aluno:"))
 
 Mediadoaluno =(Notas+Notas+Notas+Notas)/4
+
+while True:
+    #Solicita os dados do aluno
+    Mediadoaluno = float(input("digite a media do aluno:"))
+    Quantidade_faltas = int(input("digite o numero de faltas do aluno:"))
+
+    #verifica os criterio de aprovação/reprovação
+    if Quantidade_faltas > 31:
+        print("Reprovado por faltas")
+    elif Mediadoaluno >= 8:
+        print("aprovado")
+    elif Mediadoaluno >= 5:
+        print("recuperação")
+    else:
+        print("reprovado por nota")
