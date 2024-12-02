010c406de91bc14e3372a10466a5977f827dab94
jeovana barbosa <jeovana.nascimento@aluno.senai.br>
2024-12-02 09:37:05 -0500

    alteração do dia

---
+++ Revisao.py
@@ -0,0 +1,11 @@
+#Ler entradas do usuario.
+nome = input() #Armazenar o nome do aluno.
+Nota1 = float(input())#4 notas do aluno.
+Nota2 = float(input())
+Nota3 = float(input())
+Nota4 = float(input())
+
+faltas = int(input())
+#Calculo da media.
+Media = (Nota1+Nota2+Nota3+Nota4)/4
+
