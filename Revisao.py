ab838ef75997ac1de711e0a0ca4636669388570a
jeovana barbosa <jeovana.nascimento@aluno.senai.br>
2024-12-02 10:54:11 -0500

    Alteraçao finalizada

--- Revisao.py
+++ Revisao.py
@@ -9,3 +9,23 @@ faltas = int(input())
 #Calculo da media.
 Media = (Nota1+Nota2+Nota3+Nota4)/4
 
+#Logica da Situação
+if faltas > 31:
+    situacao = "Reprovado por falta"
+elif Media >= 8:
+    situacao = "aprovado"
+elif Media >= 5: #Recuperação
+    recuperacao = float(input()) # ler a nota da prova de recuperação
+    if recuperacao >= (10-Media):
+        situacao = "Aprovado na recuperação"
+    else:
+        situacao = "Reprovado na recuperação"
+else:
+    situacao = "Reprovado por media"
+
+#Relatorio
+print("nome:",nome)
+print("notas:",Nota1,Nota2,Nota3,Nota4)
+print("faltas:",faltas)
+print("media:",Media)
+print("Situacao:",situacao)
