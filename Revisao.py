e27079e36a0e81199bfea57042bd8f7921ea63c4
jeovana <jeovana.nascimento@aluno.senai.br>
2024-12-04 09:52:27 -0500

    codigo finalizado

--- Revisao.py
+++ Revisao.py
@@ -1,16 +1,19 @@
 #Ler entradas do usuario.
 #o codigo lendo varios alunos e a mesma coisa
 #a unica diferença é que ele está dentro de uma repetição 
-cont = 0 #variavel que controla a repetição.
-escolha_usuario = int (input())#variavel que guarda quantas vezes o codigo vai rodar.
-while cont < escolha_usuario:
-    nome = input() #Armazenar o nome do aluno.
-    Nota1 = float(input())#4 notas do aluno.
-    Nota2 = float(input())
-    Nota3 = float(input())
-    Nota4 = float(input())
+ #variavel que controla a repetição.
+escolha_usuario = int (input("digite quantos alunos e desja cadastrar:"))
+cont = 0 #variavel que guarda quantas vezes o codigo vai rodar.
 
-    faltas = int(input())
+Aluno =[]#lista que guardará todos os dados dos alunos cadastrado.
+while cont < escolha_usuario:
+    nome = input("digite o nome do aluno:") #Armazenar o nome do aluno.
+    Nota1 = float(input("digite a nota do 1° bimestre:"))#4 notas do aluno.
+    Nota2 = float(input("digite a nota do 2° bimestre:"))
+    Nota3 = float(input("digite a nota do 3° bimestre:"))
+    Nota4 = float(input("digite a nota do 4° bimestre:"))
+
+    faltas = int(input("digite as faltas dos aluno:"))
     #Calculo da media.
     Media = (Nota1+Nota2+Nota3+Nota4)/4
 
@@ -20,7 +23,7 @@ while cont < escolha_usuario:
     elif Media >= 8:
         situacao = "aprovado"
     elif Media >= 5: #Recuperação.
-        recuperacao = float(input()) # ler a nota da prova de recuperação.
+        recuperacao = float (input("digite a nota da recuperação:")) # ler a nota da prova de recuperação.
         if recuperacao >= (10-Media):
             situacao = "Aprovado na recuperação"
         else:
@@ -28,6 +31,9 @@ while cont < escolha_usuario:
     else:
         situacao = "Reprovado por media"
 
+#enviar os dados do aluno para a lista alunos
+    Aluno.append([nome,faltas,Media,situacao])
+    cont = cont +1
     #Relatorio.
     print("nome:",nome)
     print("notas:",Nota1,Nota2,Nota3,Nota4)
@@ -35,3 +41,5 @@ while cont < escolha_usuario:
     print("media:",Media)
     print("Situacao:",situacao)
     cont = cont +1
+print(Aluno)
+   
