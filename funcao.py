eb9c9c11d37ab5bcf3fada3b8ce8e4096843abbb
jeovana <jeovana.nascimento@aluno.senai.br>
2024-12-11 08:46:43 -0500

    novo commit

---
+++ funcao.py
@@ -0,0 +1,13 @@
+#Declarar uma funçao.
+def bom_dia():
+    print("Bom dia!")
+
+def boa_noite():
+    print("boa noite!")
+
+def boa_tarde():
+    print("boa tarde!")
+
+bom_dia()
+boa_noite()
+boa_tarde()
